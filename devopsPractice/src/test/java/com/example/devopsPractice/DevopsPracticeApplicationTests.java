06540b56ae3687e52d429bcfed56745cbcba5f77
oviyagaya <oviyabala511@gmail.com>
2026-08-08 17:20:19 +0530

    springboot-app

---
+++ devopsPractice/src/test/java/com/example/devopsPractice/DevopsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
